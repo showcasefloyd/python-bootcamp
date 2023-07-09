09b02e867556e6f8e931cd2b82669fc2d7e04e42
showcasefloyd <showcasefloyd@gmail>
2023-07-09 20:57:36 +0000

    My first Python examples

---
+++ App1/main.py
@@ -0,0 +1,11 @@
+prompt = "Enter a todo: " 
+
+to_do1 = input(prompt)
+to_do2 = input(prompt)
+to_do3 = input(prompt)
+
+todos = [to_do1, to_do2, to_do3, "More todos"]
+
+print(todos)
+
+print(type(todos))
---
+++ App1/title-length.py
@@ -0,0 +1,5 @@
+title = input("Enter a title: ")
+
+length = len(title)
+
+print("The length of the title: ", length)
